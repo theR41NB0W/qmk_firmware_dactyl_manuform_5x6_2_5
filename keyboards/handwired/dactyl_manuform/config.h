57706d0ca316c4d0095dd5374b9f6d04aefb6d1d
theR41NB0W <83562939+theR41NB0W@users.noreply.github.com>
2022-08-06 16:02:25 -0400

    Update config.h

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -1,5 +1,6 @@
 /*
-Copyright 2020 Jan Christoph Ebersbach
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,40 +20,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define MASTER_LEFT
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0x444D
+#define MANUFACTURER tshort
 
-#define SPLIT_USB_DETECT
+/* mouse config */
+#define MOUSEKEY_INTERVAL    20
+#define MOUSEKEY_DELAY       0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED   7
+#define MOUSEKEY_WHEEL_DELAY 0
 
-#define PRODUCT_ID 0x3536
-#define DEVICE_VER 0x0003
-#define PRODUCT    Dactyl-Manuform (5x6+2)
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
 
-/* key matrix size */
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 6
+/* serial.c configuration for split keyboard */
+#define SOFT_SERIAL_PIN D2
 
-// left half pro-mirco
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
 
-// // right half adafruit feather
-// #define MATRIX_COL_PINS { F1, F0, B1, B2, B3, D2 }
-// #define MATRIX_ROW_PINS { D6, B7, B6, B5, D7, C6 }
-
-#define DIODE_DIRECTION COL2ROW
-
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
-
-#define BOOTMAGIC_LITE_ROW_RIGHT 6
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
-
-// WS2812 RGB LED strip input and number of LEDs
-// #define RGB_DI_PIN D3
-// #define RGBLED_NUM 12
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
+/* Enables This makes it easier for fast typists to use dual-function keys */
+#define PERMISSIVE_HOLD
